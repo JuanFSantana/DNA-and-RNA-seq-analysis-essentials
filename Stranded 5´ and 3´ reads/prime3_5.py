f3fb823a268593e98a7a7dc4aae4e0d4b1928ff1
Juan F. Santana, Ph.D <38702786+JuanFSantana@users.noreply.github.com>
2022-04-29 08:55:21 -0500

    Update prime3_5.py

--- Stranded 5´ and 3´ reads/prime3_5.py
+++ Stranded 5´ and 3´ reads/prime3_5.py
@@ -16,24 +16,18 @@ def main(args):
     primes_list = []
     for dataset in files:
         df = pd.read_csv(dataset, sep="\t", header=None)
-        df_human = df.drop(df[df[0] == "JQCY02.1"].index).reset_index(drop=True)
-        df_human.columns = ["Chromsome", "Start", "End", "Read", "Score", "Strand"]
         
-        five_prime = df_human.copy()
-        three_prime = df_human.copy()
+        five_prime = df.copy()
+        three_prime = df.copy()
         
-        five_prime['New_Start'] = np.where(five_prime["Strand"] == "+", five_prime['Start'], five_prime['End']-1)
-        five_prime['New_End'] = np.where(five_prime["Strand"] == "+", five_prime['Start']+1, five_prime['End'])
-        cols_five = five_prime.columns.tolist()
-        cols_five = [cols_five[0]] + [cols_five[6]] + [cols_five[7]] + cols_five[3:6] 
-        five_prime = five_prime[cols_five]
+        five_prime['New_Start'] = np.where(five_prime[6] == "+", five_prime[1], five_prime[2]-1)
+        five_prime['New_End'] = np.where(five_prime[6] == "+", five_prime[1]+1, five_prime[2])
+        five_prime = five_prime[[0,'New_Start', 'New_End', 3,4,5,6]]
         primes_list.append(five_prime)
     
-        three_prime['New_Start'] = np.where(three_prime["Strand"] == "+", three_prime['End']-1, three_prime['Start'])
-        three_prime['New_End'] = np.where(three_prime["Strand"] == "+", three_prime['End'], three_prime['Start']+1)
-        cols_three = three_prime.columns.tolist()
-        cols_three = [cols_three[0]] + [cols_three[6]] + [cols_three[7]] + cols_three[3:6] 
-        three_prime = three_prime[cols_three]
+        three_prime['New_Start'] = np.where(three_prime[6] == "+", three_prime[2]-1, three_prime[1])
+        three_prime['New_End'] = np.where(three_prime[6] == "+", three_prime[2], three_prime[1]+1)
+        three_prime = three_prime[[0,'New_Start', 'New_End', 3,4,5,6]]
         primes_list.append(three_prime)
     
     for num, label in enumerate(names):
